2133f94e610c2e3390544bc4d46e113cad2fd3af
Yadla <yadlak@BO-C02VQ0GPG8WL.local>
2020-05-20 14:34:13 -0400

    adding 2 more scenario examples

--- src/test/scala/Simulations/rampUpAndDown.scala
+++ src/test/scala/Simulations/rampUpAndDown.scala
@@ -1,5 +1,36 @@
 package Simulations
 
-class rampUpAndDown {
+import baseConfig.BaseSimulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
 
+
+class rampUpAndDown extends BaseSimulation {
+  val scn = scenario(scenarioName = "Video Game DB")
+    .exec(http(requestName = "Get All Video Games - 1st call")
+      .get("videogames")
+      .check(status.is(200)))
+    .pause(2)
+
+    .exec(http(requestName = "Get Specific Game")
+      .get("videogames/1")
+      .check(status.is(200)))
+    .pause(1, 3)
+
+    .exec(http(requestName = "Get All Video Games - 2nd Call")
+      .get("videogames")
+      .check(status.is(200)))
+    .pause(2000.milliseconds)
+
+/*Below is the scenario that ramps up users..runs for some time and then ramps down users
+  * instead of using "maxDuration"- define the run duration in "constantConcurrentUsers step!*/
+
+  setUp(
+    scn.inject(
+      rampConcurrentUsers(1) to (10) during(20 seconds),
+      constantConcurrentUsers(10) during (2 minutes),
+      rampConcurrentUsers(10) to (1) during(20 seconds)
+    ).protocols(httpConf)
+  ).assertions(global.successfulRequests.percent.gt(99))
 }
--- src/test/scala/Simulations/stepUpLoadModel.scala
+++ src/test/scala/Simulations/stepUpLoadModel.scala
@@ -1,5 +1,37 @@
 package Simulations
 
-class stepUpLoadModel {
+import baseConfig.BaseSimulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
 
+
+class stepUpLoadModel extends BaseSimulation {
+  val scn = scenario(scenarioName = "Video Game DB")
+    .exec(http(requestName = "Get All Video Games - 1st call")
+      .get("videogames")
+      .check(status.is(200)))
+    .pause(2)
+
+    .exec(http(requestName = "Get Specific Game")
+      .get("videogames/1")
+      .check(status.is(200)))
+    .pause(1, 3)
+
+    .exec(http(requestName = "Get All Video Games - 2nd Call")
+      .get("videogames")
+      .check(status.is(200)))
+    .pause(2000.milliseconds)
+
+  /*Below is the scenario that ramps up users at 5, 10, 15 20 etc.,
+  */
+  setUp(
+    scn.inject(
+      incrementUsersPerSec(5)
+        .times(3)
+        .eachLevelLasting(20 seconds)
+        .separatedByRampsLasting(10 seconds)
+        .startingFrom(5)
+  ).protocols(httpConf)
+  ).assertions(global.successfulRequests.percent.gt(99))
 }
